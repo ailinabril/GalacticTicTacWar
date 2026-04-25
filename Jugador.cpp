0bff6c8bec6daa9828e4139d593d3e63899df0c3
SrTiramiZu <brunogenn3.0@gmail.com>
2026-04-25 15:37:45 -0300

    Jugador.cpp terminado
    termine las funciones mostrar y cargar. configure los getters y setters

--- Jugador.cpp
+++ Jugador.cpp
@@ -25,3 +25,69 @@ Jugador::Jugador(){
     _eliminado = false;
     strcpy (_nombre, nomb);
 }
+
+///cargar datos///
+void Jugador::Cargar(int id) {
+    _idJugador = id;
+
+    cout << "Ingrese nombre del jugador: ";
+    cin.ignore();
+    cin.getline(_nombre, 30);
+
+    _victorias = 0;
+    _derrotas = 0;
+    _empates = 0;
+    _eliminado = false;
+}
+
+///mostrar datos///
+void Jugador::Mostrar() {
+    cout << "ID: " << _idJugador << endl;
+    cout << "Nombre: " << _nombre << endl;
+    cout << "Victorias: " << _victorias << endl;
+    cout << "Derrotas: " << _derrotas << endl;
+    cout << "Empates: " << _empates << endl;
+    cout << "Eliminado: " << (_eliminado ? "Si" : "No") << endl;
+}
+
+///getters///
+int Jugador::getIdJugador() {
+    return _idJugador;
+}
+
+const char* Jugador::getNombre() {
+    return _nombre;
+}
+
+int Jugador::getVictorias() {
+    return _victorias;
+}
+
+int Jugador::getDerrotas() {
+    return _derrotas;
+}
+
+int Jugador::getEmpates() {
+    return _empates;
+}
+
+bool Jugador::getEliminado() {
+    return _eliminado;
+}
+
+///setters///
+void Jugador::setVictorias(int victorias) {
+    _victorias = victorias;
+}
+
+void Jugador::setDerrotas(int derrotas) {
+    _derrotas = derrotas;
+}
+
+void Jugador::setEmpates(int empates) {
+    _empates = empates;
+}
+
+void Jugador::setEliminado(bool estado) {
+    _eliminado = estado;
+}
