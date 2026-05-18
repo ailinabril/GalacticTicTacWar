0fa51a1f37bab011edae7ab9a2ea1556b118f902
ailinabril <ailinaguirreabril.521@gmail.com>
2026-05-18 20:02:17 -0300

    subi la parte de bomba de bruno porque no se veia

--- Bomba.cpp
+++ Bomba.cpp
@@ -1,1 +1,21 @@
+#include "Bomba.h"
 
+///constructor///
+Bomba::Bomba() : ObjetoEspecial(4, "Bomba"){
+}
+
+///destruye una ficha enemiga///
+bool Bomba::aplicarEfecto(Tablero &tablero, int fila, int columna, char simboloJugador){
+    char casilla = tablero.getCasillero(fila, columna);
+
+    ///no se puede usar sobre una ficha propia///
+    if (casilla == simboloJugador)
+    {
+        return false;
+    }
+
+    ///destruye la casilla///
+    tablero.setCasillero(fila, columna, '*');
+
+    return true;
+}
